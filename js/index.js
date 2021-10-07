18a907a29ae2eb641fd1c0294cdf149e342fcdf6
RafaelLimaReis <rafaellimareis01@gmail.com>
2021-10-06 23:07:59 -0300

    mobile v1

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
-document.getElementById('scrollTop').addEventListener('click', () => {
-    location.href = location.origin + location.pathname + '#main';
-    icon.style.display = 'flex';
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
 const icon = document.getElementById('nextSection');
+var menuIsEnable = false;
 var on = false;
+
+
+document.getElementById('scrollTop').addEventListener('click', toTop);
+
+document.getElementById('menu__mobile').addEventListener('click', controlMenu);
 
 setInterval(function () {
     document.getElementById('nextSection').style.animation = (on) ? 'topToBottom 1s 0s linear infinite' : '';
     on = !on;
 }, 3000);
 
+[...document.getElementsByClassName('menu__item a')].forEach(() => {
+    const urlHash = window.location.hash;
+
+    stateAngle(urlHash);
+})
+
 icon.addEventListener('click', () => {
     const urlHash = window.location.hash;
     if (urlHash == '') {
         location.href += '#' + document.getElementsByTagName('section')[1].id;
     } else {
-        [...document.getElementsByTagName('section')].forEach((element, i, arr) => {
-            if (`#${element.id}` == urlHash) {
-                if (arr.length == i+2) {
-                    icon.style.display = 'none';
-                }
-                location.href = location.origin + location.pathname + '#' + document.getElementsByTagName('section')[i+1].id;
-            }
-        });
+        stateAngle(urlHash);
     }
 });
 
@@ -39,3 +37,35 @@ const sr = ScrollReveal({
 sr.reveal('.animate-d1', {delay: 200});
 sr.reveal('.animate-d2', {delay: 400});
 sr.reveal('.animate-d3', {delay: 600});
+
+
+function toTop()
+{
+    location.href = location.origin + location.pathname + '#main';
+    icon.style.display = 'flex';
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function controlMenu()
+{
+    menuIsEnable = !menuIsEnable;
+    document.getElementsByClassName('menu__list')[0].classList.toggle('active');
+    [...document.getElementsByClassName('menu__item')].forEach((element) => {
+        element.style.animation = (menuIsEnable) ? 'rightToLeft .5s' : null;
+    });
+}
+
+function stateAngle(urlHash)
+{
+    [...document.getElementsByTagName('section')].forEach((element, i, arr) => {
+        if (`#${element.id}` == urlHash) {
+            if (arr.length == i+2) {
+                icon.style.display = 'none';
+            } else {
+                icon.style.display = 'flex';
+            }
+            location.href = location.origin + location.pathname + '#' + document.getElementsByTagName('section')[i+1].id;
+        }
+    });
+}
